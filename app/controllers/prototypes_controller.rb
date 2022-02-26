0db48715527adbeaf8dbc6541e7fb61e3f7c9016
sakoji14 <sj.yu.sato1424@gmail.com>
2022-02-26 14:20:23 +0900

    first commit

---
+++ app/controllers/prototypes_controller.rb
@@ -0,0 +1,60 @@
+class PrototypesController < ApplicationController
+  before_action :authenticate_user!, except: [:show, :index]
+  before_action :move_to_index, except: [:index, :show]
+
+  def index
+    @prototypes = Prototype.all
+  end
+
+  def new
+    @prototype = Prototype.new
+  end
+
+  def create
+    @prototype = Prototype.new(prototype_params)
+    if @prototype.save
+      redirect_to root_path(id: current_user)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @prototype = Prototype.find(params[:id])
+    @comment = Comment.new
+    @comments = @prototype.comments.includes(:user)
+  end
+
+  def edit
+  end
+
+  def update
+    @prototype = Prototype.find(params[:id])
+    if @prototype.update(prototype_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @prototype = Prototype.find(params[:id])
+    if @prototype.destroy
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
+  end
+
+
+  private
+
+  def prototype_params
+    params.require(:prototype).permit(:catch_copy, :title, :image, :concept).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    @prototypes = Prototype.find(params[:id])
+    redirect_to root_path unless current_user == @prototypes.user
+  end
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,6 @@
+class UsersController < ApplicationController
+  def show
+    @name = current_user.name
+    @prototypes = current_user.prototypes
+  end
+end
